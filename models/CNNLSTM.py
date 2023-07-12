3d472192080d4baa1c76187fd0d911028caabb8f
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-07-12 23:36:29 +0900

    Fix to pass cropping images to LSTM

--- models/CNNLSTM.py
+++ models/CNNLSTM.py
@@ -20,23 +20,23 @@ class CNNLSTM(nn.Module):
         self.lstm = nn.LSTM(input_size=8192, hidden_size=64, num_layers=2, batch_first=True)
         self.fc = nn.Linear(64, 2)
 
-    def forward(self, inputs):
-        crops = []
-        for x in inputs:
-            batch_size, channels, height, width = x.size()
+    def forward(self, batch_i):
+        batch_o = []
+        for x in batch_i:
+            crop_size, channels, height, width = x.size()
 
-            # 画像データの処理
-            x = x.view(batch_size, channels, height, width)
+            # クロップ画像の処理
+            x = x.view(crop_size, channels, height, width)
             x = self.cnn(x)
-            x = x.view(batch_size, -1)
-            crops.append(x)
+            x = x.view(crop_size, -1)
 
-        crops = torch.stack(crops)
+            # LSTM処理をして最後の出力を取得
+            _, (h_n, _) = self.lstm(x)
+            x = h_n[-1]
 
-        # LSTM処理をして最後の出力を取得
-        _, (h_n, _) = self.lstm(crops)
-        x = h_n[-1]
+            # LSTM層の出力から2値に分類
+            x = self.fc(x)
+            batch_o.append(x)
 
-        # LSTM層の出力から2値に分類
-        x = self.fc(x)
-        return x
+        batch_o = torch.stack(batch_o)
+        return batch_o
