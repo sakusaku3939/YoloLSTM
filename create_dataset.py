3921bc06fd5104ceef4621b6799322821b62298e
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-08-13 09:40:44 +0900

    Change the format of the dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,24 +1,24 @@
 import os
-import csv
+import shutil
 
-path = "./data/train/0627/"
+source_path = "./data/0627/"
+output_path = "./data/train/"
 
 # X * y = 13 * 12
 # 推定で使用する座標を、撮影する順番（右下から左上）に記録
 pos_list = [range(1, 8), range(1, 12), range(6, 13), range(6, 8), range(6, 8), range(6, 8), range(6, 8), range(6, 8),
             range(1, 13), range(6, 8), range(6, 8), range(6, 8), range(1, 13)]
 
-file_names = sorted(os.listdir(path))
-x = 1
-outputs = []
+file_names = iter(sorted(os.listdir(source_path)))
 
-# (image_name, x, y) のラベルデータを作成
-for y_list in pos_list:
+for x, y_list in enumerate(pos_list, start=1):
     for i, y in enumerate(y_list):
-        outputs.append([file_names[i], x, y])
-    x += 1
+        # 座標毎のフォルダを作成
+        folder_path = output_path + f"{x}_{y}"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
-# CSVに書き込み
-with open(path + "label.csv", 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(outputs)
+        # 画像ファイルをfolder_pathに移動
+        file_name = next(file_names)
+        shutil.copy(source_path + file_name, folder_path + "/" + file_name)
+
