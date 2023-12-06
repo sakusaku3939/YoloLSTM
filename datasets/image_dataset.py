3fddd5559d74f0bf521d2a7fb7eaa283b492bd22
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-12-06 23:36:12 +0900

    Refactoring

--- datasets/image_dataset.py
+++ datasets/image_dataset.py
@@ -8,32 +8,31 @@ from PIL import Image
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root, transform) -> None:
+    def __init__(self, root, transform=None):
         super().__init__()
         self.dataset = []
 
         # datasetにラベルと画像パスを追加
         dir_names = sorted(os.listdir(root))
         for d_name in dir_names:
-            position = [float(p) for p in re.findall(r'\d+', d_name)]
+            xy = [float(p) for p in re.findall(r'\d+', d_name)]
             file_paths = glob.glob(f"{root}/{d_name}/*")
             for f_path in file_paths:
-                self.dataset.append({"target": position, "f_path": f_path})
+                self.dataset.append({"position": xy, "path": f_path})
 
         self.root = root
         self.transform = transform
 
     def __getitem__(self, index):
-        f_path = self.dataset[index]["f_path"]
-
+        f_path = self.dataset[index]["path"]
         img = Image.open(f_path)
 
         # 画像を前処理
         if self.transform is not None:
             img = self.transform(img)
 
-        target = self.dataset[index]["target"]
-        return img, torch.tensor(target)
+        xy = self.dataset[index]["position"]
+        return img, torch.tensor(xy)
 
     def __len__(self):
         return len(self.dataset)
