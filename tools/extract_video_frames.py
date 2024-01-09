ed522eb514e52324cc110b271b74ce7244de5e5f
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-01-09 18:27:23 +0900

    Update to allow creating multiple video datasets

--- tools/extract_video_frames.py
+++ tools/extract_video_frames.py
@@ -3,15 +3,23 @@ import os
 import cv2
 from tqdm import tqdm
 
-capture = cv2.VideoCapture("./1_1.mp4")
-dir_path = "./images"
-os.makedirs(dir_path, exist_ok=True)
+input_dir = "../inputs"
+output_dir = "./images"
 
-for n in tqdm(range(0, int(capture.get(cv2.CAP_PROP_FRAME_COUNT)))):
-    ret, frame = capture.read()
-    # 読み込めなかった場合はループを抜ける
-    if not ret:
-        break
+for file_name in os.listdir(input_dir):
+    video_path = os.path.join(input_dir, file_name)
+    capture = cv2.VideoCapture(video_path)
 
-    if int(n % 6) == 0:
-        cv2.imwrite(f"./{dir_path}/{int(n // 6)}.png", frame)
+    # 出力フォルダを作成
+    video_output_dir = os.path.join(output_dir, file_name.split('.')[0])
+    os.makedirs(video_output_dir, exist_ok=True)
+
+    for n in tqdm(range(int(capture.get(cv2.CAP_PROP_FRAME_COUNT)))):
+        ret, frame = capture.read()
+        # 読み込めなかった場合はループを抜ける
+        if not ret:
+            break
+
+        if int(n % 6) == 0:
+            frame_file = os.path.join(video_output_dir, f"{int(n // 6)}.png")
+            cv2.imwrite(frame_file, frame)
